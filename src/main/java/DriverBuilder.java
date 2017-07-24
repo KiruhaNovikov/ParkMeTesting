6e94f2bbee36008f5087ad1e2fd523142af1dda4
Answeroid <send.mail.to.flash@gmail.com>
2017-07-24 16:53:35 +0300

    multiple web-driver feature and started work with properties

--- src/main/java/DriverBuilder.java
+++ src/main/java/DriverBuilder.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.phantomjs.*;
+import org.openqa.selenium.chrome.*;
 import java.util.concurrent.TimeUnit;
 
 
@@ -9,12 +11,26 @@ public enum DriverBuilder {
     private WebDriver driver;
 
     DriverBuilder(){
-        System.setProperty("webdriver.chrome.driver", "chromedriver"); // needed to be manually set for vkhalin
-        try{
-            driver = new ChromeDriver();
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        }catch (Exception e){
-            throw new ExceptionInInitializerError(e);
+        String driverName = System.getProperty("driverName");
+        if (driverName.equals("phantomjs")){
+            try{
+                DesiredCapabilities caps = new DesiredCapabilities();
+                caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "phantomjs");
+                driver = new PhantomJSDriver();
+                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            }catch (Exception e){
+                throw new ExceptionInInitializerError(e);
+            }
+        } else {
+            System.setProperty("webdriver.chrome.driver", "chromedriver");
+            ChromeOptions options = new ChromeOptions();
+            try{
+                driver = new ChromeDriver();
+                options.addArguments("--headless");
+                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            }catch (Exception e){
+                throw new ExceptionInInitializerError(e);
+            }
         }
     }
 
--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,23 +1,14 @@
-import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-
-import java.util.concurrent.TimeUnit;
 
 
 public class TestBase {
 
-    WebDriver driver;
+    WebDriver driver = DriverBuilder.INSTANCE.getDriver();
 
     @BeforeTest
     public void openParkMeHomePage() {
-        FirefoxDriverManager.getInstance().setup();
-        driver = new ChromeDriver();
-        /*ChromeDriverManager.getInstance().setup();
-        driver = new ChromeDriver();*/
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://parkme.com/");
     }
 
